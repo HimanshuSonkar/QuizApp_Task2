5ff3d9bf183936450597321974d6e9921b471cf3
HimanshuSonkar <92172389+HimanshuSonkar@users.noreply.github.com>
2023-07-25 12:51:03 +0530

    Add files via upload
    Initial Commit

---
+++ questions.js
@@ -0,0 +1,65 @@
+let questions = [
+    {
+        numb:1,
+        question:"HTML stands for -",
+        answer:"C. HyperText Markup Language",
+        options: [
+            "A. HighText Machine Language",
+            "B. HyperText and links Markup Language",
+            "C. HyperText Markup Language",
+            "D.None of these"
+        ]
+    }, 
+    {
+        numb:2,
+        question:"The correct sequence of HTML tags for starting a webpage is -",
+        answer:"D. HTML, Head, Title, Body",
+        options: [
+            "A. Head, Title, HTML, body",
+            "B. HTML, Body, Title, Head",
+            "C. HTML, Head, Title, Body",
+            "D. HTML, Head, Title, Body"
+
+        ]
+
+    }, 
+    {
+        numb:3,
+        question:"Which of the following element is responsible for making the text bold in HTML?",
+        answer:"C. b",
+        options: [
+            "A. pre",
+            "B. a",
+            "C. b",
+            "D. br"
+
+        ]
+
+    }, 
+    {
+        numb:4,
+        question:"Which of the following tag is used for inserting the largest heading in HTML?",
+        answer:"B. h1",
+        options: [
+            "A. h3",
+            "B. h1",
+            "C. h5",
+            "D. h6"
+
+        ]
+
+    }, 
+    {
+        numb:5,
+        question:"Which of the following tag is used to insert a line-break in HTML?",
+        answer:"A. br",
+        options: [
+            "A. br",
+            "B. a",
+            "C. pre",
+            "D. b"
+
+        ]
+
+    }, 
+]
